f8492382aaad6ffe1b28a6a2ff9f3ae13d25f70e
haidongshi <hidgshi@gmail.com>
2023-10-29 15:23:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,18 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "blog" {
   name = "blog"
   tags = {
